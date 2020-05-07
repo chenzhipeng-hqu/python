68fed6e799033883dfc94058ce0b90f2f8af0cff
chenzhipeng3472 <chenzhipeng3472@cvte.com>
2020-05-07 17:20:18 +0800

    [feature][FinancialTools][practice] add practice3
    [what] 添加practice3
    [why]null
    [how] 修改数据, 不破坏其他单元格内容

--- practice/practice.py
+++ practice/practice.py
@@ -17,13 +17,16 @@ import pyperclip
 import xml.sax
 import configparser
 import pandas as pd
+import openpyxl
 import financial_ui as ui
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 
-logging.basicConfig(level=logging.DEBUG,  filename='out.log',
-                    datefmt='%Y/%m/%d %H:%M:%S',
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(module)s - %(message)s')
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     filename='out.log',
+#     datefmt='%Y-%m-%d %H:%M:%S',
+#     format='%(asctime)s - %(levelname)s - %(filename)s - %(module)s - %(funcName)s - %(lineno)d - %(message)s')
 
 logger = logging.getLogger(__name__)
 
@@ -55,8 +58,102 @@ class Practice(QObject):
         print(df.head())
         print('Done!')
 
+    def practice3(self):
+        # fw = pd.DataFrame({'A': [1, 2]})
+        #
+        # fw1 = pd.DataFrame({'B': [3, 4]})
+        #
+        # fw2 = pd.DataFrame({'C': [5, 6]})
+        #
+        target_path = r'datas/practice3.xlsx'
+        #
+        # append_df_to_excel(target_path, fw, sheet_name='Sheet1', startcol=0, startrow=0, index=False, header=False)
+        #
+        # append_df_to_excel(target_path, fw1, sheet_name='Sheet1', startcol=1, startrow=0, index=False, header=False)
+        #
+        # append_df_to_excel(target_path, fw2, sheet_name='Sheet3', startcol=3, startrow=5, index=False, header=False)
+
+        df = pd.read_excel(target_path, sheet_name='Sheet2', header=1)
+        src_data = df['10月']
+        print(src_data[:6])
+        append_df_to_excel(target_path, src_data[:6], sheet_name='Sheet2', startcol=6, startrow=2, index=False,
+                           header=False)
+
+
+def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
+                       truncate_sheet=False,
+                       **to_excel_kwargs):
+    """
+    Append a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+    If [filename] doesn't exist, then this function will create it.
+    Parameters:
+      filename : File path or existing ExcelWriter
+                 (Example: '/path/to/file.xlsx')
+      df : dataframe to save to workbook
+      sheet_name : Name of sheet which will contain DataFrame.
+                   (default: 'Sheet1')
+      startrow : upper left cell row to dump data frame.
+                 Per default (startrow=None) calculate the last row
+                 in the existing DF and write to the next row...
+      truncate_sheet : truncate (remove and recreate) [sheet_name]
+                       before writing DataFrame to Excel file
+      to_excel_kwargs : arguments which will be passed to `DataFrame.to_excel()`
+                        [can be dictionary]
+    Returns: None
+    """
+    # from openpyxl import load_workbook
+
+    # import pandas as pd
+
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl')
+
+    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist
+    try:
+        FileNotFoundError
+    except NameError:
+        FileNotFoundError = IOError
+
+    try:
+        # try to open an existing workbook
+        writer.book = openpyxl.load_workbook(filename)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if startrow is None:
+        startrow = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save()
 
 if __name__ == '__main__':
     practice = Practice()
     # practice.practice1()
-    practice.practice2()
+    # practice.practice2()
+    practice.practice3()
+
