0c1b230b563d54bf3f2c142ef841d2f23ac9281a
chenzhipeng3472 <chenzhipeng3472@cvte.com>
2020-05-30 10:15:58 +0800

    [feature][common][tests] modify test_main.py
    [what] 修改 test_main.py 里面的内容
    [why]null
    [how]null

---
+++ tests/test_main.py
@@ -0,0 +1,37 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/03/26
+# @Author  : 陈志鹏
+# @File    : xxx.py
+
+import unittest
+from project import log
+# from test_all import *
+
+logger = log.Log(__name__).getlog()
+
+def test_suite():
+    '''
+    https://blog.csdn.net/xiaoquantouer/article/details/75089200
+    '''
+
+    suite = unittest.TestSuite()
+
+    # tests = [TestDict("test_init"),
+    #          TestDict("test_key"),
+    #          TestDict("test_attr"),
+    #          TestDict("test_keyerror"),
+    #          TestDict("test_attrerror")]
+    #
+    # suite.addTests(tests)
+
+    with open('UnittestTextReport.txt', 'w') as f:
+        runner = unittest.TextTestRunner(stream=f, verbosity=2)
+        runner.run(suite)
+
+    # runner = unittest.TextTestRunner(verbosity=2)
+    # runner.run(suite)
+
+
+if __name__ == '__main__':
+    test_suite()
