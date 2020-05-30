9021a97fee5b97db7a5487d84ef9277130782f18
chenzhipeng3472 <chenzhipeng3472@cvte.com>
2020-05-30 11:32:23 +0800

    [feature][common][framework] add python project framework
    [what] 添加python的项目框架
    [why]null
    [how]null

---
+++ docs/README.md
@@ -0,0 +1,19 @@
+
+# install
+```
+获取源码库
+sudo wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+
+解压进入目录
+tar -zxvf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+
+编译安装
+sudo ./configure --prefix=/usr  
+sudo make
+sudo make install
+
+重新安装python的TA-Lib库
+pip install TA-Lib
+
+```
---
+++ project/main.py
@@ -0,0 +1,24 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/05/30
+# @Author  : 陈志鹏
+# @File    : xx.py
+"""
+
+"""
+
+__author__ = '陈志鹏'
+
+import os
+import sys
+work_path = os.path.join(os.path.dirname(sys.argv[0]), "../")
+sys.path.append(os.path.abspath(work_path))
+os.chdir(work_path)
+
+from project import log
+
+logger = log.Log(__name__, log_path=os.getcwd()).getlog()
+
+
+if __name__ == '__main__':
+    logger.info('\r\n ---------------- welcom to use -----------------')
