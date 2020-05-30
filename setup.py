a61b4c9702a12889d50969fa2123cfbca9fa75a6
chenzhipeng3472 <chenzhipeng3472@cvte.com>
2020-05-31 00:14:51 +0800

    [feature][common][setup] add setup function and script
    [what]
    [why]null
    [how]null

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+
+from setuptools import setup
+
+"""
+https://blog.csdn.net/chenfeidi1/article/details/80873979
+"""
+
+setup(
+    name='candle',
+    version='0.0.1',
+    packages=['project'],
+)
