dc8987153818c92d10259e77e7de5749f610d4ed
chenzhipeng3472 <chenzhipeng3472@cvte.com>
2020-06-01 01:02:31 +0800

    [feature][candle][practice] add practice2.py
    [what]
    [why]null
    [how]null

--- practice/practice1.py
+++ practice/practice1.py
@@ -9,10 +9,16 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../")))
 
 import tushare as ts
+import  matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter, WeekdayLocator, DayLocator, MONDAY, date2num
+# from matplotlib.finance import candlestick_ohlc
+# from mpl_finance import candlestick_ohlc
+import mplfinance as mpf
 import talib as ta
 import pandas as pd
 from project import log
+from cycler import cycler# 用于定制线条颜色
 
 logger = log.Log(__name__).getlog()
 
@@ -78,6 +84,146 @@ def test2():
 
     plt.show()
 
+    # candlePlot(df, title='2020 K Plot')
+
+# def candlePlot(seriesData, title="a"):
+#     # 设定日期格式
+#     Date = [date2num(date) for date in seriesData.index]
+#     seriesData.loc[:, 'Date'] = Date
+#     listData = []
+#     for i in range(len(seriesData)):
+#         a = [seriesData.Date[i], \
+#              seriesData.open[i], seriesData.high[i], \
+#              seriesData.low[i], seriesData.close[i]]
+#         listData.append(a)
+#
+#     # 设定绘图相关参数
+#     ax = plt.subplot()
+#     mondays = WeekdayLocator(MONDAY)
+#     # 日期格式为‘15-Mar-09’形式
+#     weekFormatter = DateFormatter('%y %b %d')
+#     ax.xaxis.set_major_locator(mondays)
+#     ax.xaxis.set_minor_locator(DayLocator())
+#     ax.xaxis.set_major_formatter(weekFormatter)
+#
+#     # 调用candlestick_ohlc函数
+#     candlestick_ohlc(ax, listData, width=0.7, \
+#                      colorup='r', colordown='g')
+#     ax.set_title(title)  # 设定标题
+#     # 设定x轴日期显示角度
+#     plt.setp(plt.gca().get_xticklabels(), \
+#              rotation=50, horizontalalignment='center')
+#     return (plt.show())
+
+
+# 蜡烛图与线图
+# def candleLinePlots(candleData, candleTitle='a', **kwargs):
+#     Date = [date2num(date) for date in candleData.index]
+#     candleData.loc[:, 'Date'] = Date
+#     listData = []
+#
+#     for i in range(len(candleData)):
+#         a = [candleData.Date[i], \
+#              candleData.Open[i], candleData.High[i], \
+#              candleData.Low[i], candleData.Close[i]]
+#         listData.append(a)
+#     # 如 果 不 定 长 参 数 无 取 值 ， 只 画 蜡 烛 图
+#     ax = plt.subplot()
+#
+#     # 如 果 不 定 长 参 数 有 值 ， 则 分 成 两 个 子 图
+#     flag = 0
+#     if kwargs:
+#         if kwargs['splitFigures']:
+#             ax = plt.subplot(211)
+#             ax2 = plt.subplot(212)
+#             flag = 1;
+#         # 如 果 无 参 数 splitFigures ， 则 只 画 一 个 图 形 框
+#         # 如 果 有 参 数 splitFigures ， 则 画 出 两 个 图 形 框
+#
+#         for key in kwargs:
+#             if key == 'title':
+#                 ax2.set_title(kwargs[key])
+#             if key == 'ylabel':
+#                 ax2.set_ylabel(kwargs[key])
+#             if key == 'grid':
+#                 ax2.grid(kwargs[key])
+#             if key == 'Data':
+#                 plt.sca(ax)
+#                 if flag:
+#                     plt.sca(ax2)
+#
+#                 # 一维数据
+#                 if kwargs[key].ndim == 1:
+#                     plt.plot(kwargs[key], \
+#                              color='k', \
+#                              label=kwargs[key].name)
+#                     plt.legend(loc='best')
+#                 # 二维数据有两个columns
+#                 elif all([kwargs[key].ndim == 2, \
+#                           len(kwargs[key].columns) == 2]):
+#                     plt.plot(kwargs[key].iloc[:, 0], color='k',
+#                              label=kwargs[key].iloc[:, 0].name)
+#                     plt.plot(kwargs[key].iloc[:, 1], \
+#                              linestyle='dashed', \
+#                              label=kwargs[key].iloc[:, 1].name)
+#                     plt.legend(loc='best')
+#
+#     mondays = WeekdayLocator(MONDAY)
+#     weekFormatter = DateFormatter('%y %b %d')
+#     ax.xaxis.set_major_locator(mondays)
+#     ax.xaxis.set_minor_locator(DayLocator())
+#     ax.xaxis.set_major_formatter(weekFormatter)
+#     plt.sca(ax)
+#
+#     candlestick_ohlc(ax, listData, width=0.7, \
+#                      colorup='r', colordown='g')
+#     ax.set_title(candleTitle)
+#     plt.setp(ax.get_xticklabels(), \
+#              rotation=20, \
+#              horizontalalignment='center')
+#     ax.autoscale_view()
+#
+#     return (plt.show())
+
+
+# 蜡烛图与成交量柱状图
+# def candleVolume(seriesData, candletitle='a', bartitle='b'):
+#     Date = [date2num(date) for date in seriesData.index]
+#     seriesData.index = list(range(len(Date)))
+#     seriesData['Date'] = Date
+#     listData = zip(seriesData.Date, seriesData.Open, seriesData.High, seriesData.Low,
+#                    seriesData.Close)
+#     ax1 = plt.subplot(211)
+#     ax2 = plt.subplot(212)
+#     for ax in ax1, ax2:
+#         mondays = WeekdayLocator(MONDAY)
+#         weekFormatter = DateFormatter('%m/%d/%Y')
+#         ax.xaxis.set_major_locator(mondays)
+#         ax.xaxis.set_minor_locator(DayLocator())
+#         ax.xaxis.set_major_formatter(weekFormatter)
+#         ax.grid(True)
+#
+#     ax1.set_ylim(seriesData.Low.min() - 2, seriesData.High.max() + 2)
+#     ax1.set_ylabel('蜡烛图及收盘价线')
+#     candlestick_ohlc(ax1, listData, width=0.7, colorup='r', colordown='g')
+#     plt.setp(plt.gca().get_xticklabels(), \
+#              rotation=45, horizontalalignment='center')
+#     ax1.autoscale_view()
+#     ax1.set_title(candletitle)
+#     ax1.plot(seriesData.Date, seriesData.Close, \
+#              color='black', label='收盘价')
+#     ax1.legend(loc='best')
+#
+#     ax2.set_ylabel('成交量')
+#     ax2.set_ylim(0, seriesData.Volume.max() * 3)
+#     ax2.bar(np.array(Date)[np.array(seriesData.Close >= seriesData.Open)]
+#             , height=seriesData.iloc[:, 4][np.array(seriesData.Close >= seriesData.Open)]
+#             , color='r', align='center')
+#     ax2.bar(np.array(Date)[np.array(seriesData.Close < seriesData.Open)]
+#             , height=seriesData.iloc[:, 4][np.array(seriesData.Close < seriesData.Open)]
+#             , color='g', align='center')
+#     ax2.set_title(bartitle)
+#     return (plt.show())
 
 # def test2():
 #     ta.BBANDS(close, timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
@@ -108,6 +254,122 @@ def test2():
 #
 #     plt.show()
 
+def test3():
+    """
+        https://blog.csdn.net/qq_41437512/article/details/105319421
+    """
+    # 导入数据
+    symbol = '603976'
+    df = ts.get_k_data(symbol, start='2020-01-12')
+
+    # print(df.head())
+
+    # 格式化列名，用于之后的绘制
+    df.rename(
+        columns={
+            'date': 'Date', 'open': 'Open',
+            'high': 'High', 'low': 'Low',
+            'close': 'Close', 'volume': 'Volume'},
+        inplace=True)
+    # 转换为日期格式
+    df['Date'] = pd.to_datetime(df['Date'])
+    # 将日期列作为行索引
+    df.set_index(['Date'], inplace=True)
+
+    # print(df.head())
+
+    # 设置基本参数
+    # type:绘制图形的类型，有candle, renko, ohlc, line等
+    # 此处选择candle,即K线图
+    # mav(moving average):均线类型,此处设置7,30,60日线
+    # volume:布尔类型，设置是否显示成交量，默认False
+    # title:设置标题
+    # y_label:设置纵轴主标题
+    # y_label_lower:设置成交量图一栏的标题
+    # figratio:设置图形纵横比
+    # figscale:设置图形尺寸(数值越大图像质量越高)
+    kwargs = dict(
+        type='candle',
+        mav=(5, 10, 20),
+        volume=True,
+        title='\nA_stock %s candle_line' % (symbol),
+        ylabel='OHLC Candles',
+        ylabel_lower='Shares\nTraded Volume',
+        figratio=(15, 10),
+        figscale=5)
+
+    # 设置marketcolors
+    # up:设置K线线柱颜色，up意为收盘价大于等于开盘价
+    # down:与up相反，这样设置与国内K线颜色标准相符
+    # edge:K线线柱边缘颜色(i代表继承自up和down的颜色)，下同。详见官方文档)
+    # wick:灯芯(上下影线)颜色
+    # volume:成交量直方图的颜色
+    # inherit:是否继承，选填
+    mc = mpf.make_marketcolors(
+        up='red',
+        down='green',
+        edge='i',
+        wick='i',
+        volume='in',
+        inherit=True)
+
+    # 设置图形风格
+    # gridaxis:设置网格线位置
+    # gridstyle:设置网格线线型
+    # y_on_right:设置y轴位置是否在右
+    s = mpf.make_mpf_style(
+        gridaxis='both',
+        gridstyle='-.',
+        y_on_right=False,
+        marketcolors=mc)
+
+    # 设置均线颜色，配色表可见下图
+    # 建议设置较深的颜色且与红色、绿色形成对比
+    # 此处设置七条均线的颜色，也可应用默认设置
+    mpl.rcParams['axes.prop_cycle'] = cycler(
+        color=['dodgerblue', 'goldenrod', 'deeppink',
+               'navy', 'teal', 'maroon', 'darkorange',
+               'indigo'])
+
+    # 设置线宽
+    mpl.rcParams['lines.linewidth'] = .5
+
+    # 图形绘制
+    # show_nontrading:是否显示非交易日，默认False
+    # savefig:导出图片，填写文件名及后缀
+    period = '5'
+    mpf.plot(df,
+             **kwargs,
+             style=s,
+             show_nontrading=False,
+                savefig='A_stock-%s %s_candle_line'
+                 % (symbol, period) + '.png')
+    plt.show()
+
+def test4():
+
+    df = ts.get_k_data('603976', start='2020-01-12')
+    # df = ts.get_k_data('603976')
+
+    df.index = pd.to_datetime(df.date)
+
+    df = df.sort_index()
+
+    # 1. Two Crows 两只乌鸦
+    integer = ta.CDL2CROWS(df.open, df.high, df.low, df.close)
+    print(integer[integer > 0])
+
+    integer = ta.CDL3BLACKCROWS(df.open, df.high, df.low, df.close)
+    print(integer[integer > 0])
+
+    integer = ta.CDL3INSIDE(df.open, df.high, df.low, df.close)
+    print(integer[integer > 0])
+
+    integer = ta.CDL3INSIDE(df.open, df.high, df.low, df.close)
+    print(integer[integer > 0])
+
+
 if __name__ == '__main__':
     logger.info('\r\n welcom to use candle predict')
-    test2()
+    # test3()
+    test4()
---
+++ practice/practice2.py
@@ -0,0 +1,480 @@
+# -*- coding=utf-8 -*-
+
+__author__ = 'Rocky'
+
+'''
+
+http://30daydo.com
+
+Contact: weigesysu@qq.com
+
+'''
+
+# 每天的涨跌停
+
+import re
+
+import time
+
+import xlrd
+
+import xlwt
+
+import sys
+
+import os
+
+# import settings
+#
+# from settings import is_holiday, DATA_PATH
+
+import pandas as pd
+
+# from settings import llogger,get_mysql_conn,get_engine
+
+import requests
+
+# from send_mail import sender_139
+
+import datetime
+
+from project import log
+
+import tushare as ts
+
+
+
+# logger = llogger('log/zdt.log')
+logger = log.Log(__name__).getlog()
+
+DATA_PATH = os.getcwd()
+
+class GetZDT(object):
+
+
+
+    def __init__(self,today):
+
+        self.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/64.0.3282.167 Chrome/64.0.3282.167 Safari/537.36"
+
+
+
+        if today is None:
+
+            self.today = time.strftime("%Y%m%d")
+
+        else:
+
+            self.today = today
+
+
+
+        self.path = DATA_PATH
+
+        self.zdt_url = 'http://home.flashdata2.jrj.com.cn/limitStatistic/ztForce/' +  self.today + ".js"
+
+        self.zrzt_url = 'http://hqdata.jrj.com.cn/zrztjrbx/limitup.js'
+
+
+
+        self.host = "home.flashdata2.jrj.com.cn"
+
+        self.reference = "http://stock.jrj.com.cn/tzzs/zdtwdj/zdforce.shtml"
+
+
+
+        self.header_zdt = {"User-Agent": self.user_agent,
+
+                           "Host": self.host,
+
+                           "Referer": self.reference}
+
+
+
+        self.zdt_indexx = ['代码', '名称', '最新价格', '涨跌幅', '封成比', '封流比', '封单金额', '最后一次涨停时间', '第一次涨停时间', '打开次数',
+
+                           '振幅',
+
+                           '涨停强度']
+
+
+
+        self.zrzt_indexx = ['序号', '代码', '名称', '昨日涨停时间', '最新价格', '今日涨幅', '最大涨幅', '最大跌幅', '是否连板', '连续涨停次数',
+
+                            '昨日涨停强度', '今日涨停强度', '是否停牌', '昨天的日期', '昨日涨停价', '今日开盘价格', '今日开盘涨幅']
+
+        self.header_zrzt = {"User-Agent": self.user_agent,
+
+                            "Host": "hqdata.jrj.com.cn",
+
+                            "Referer": "http://stock.jrj.com.cn/tzzs/zrztjrbx.shtml"
+
+                            }
+
+
+
+    def getdata(self, url, headers, retry=5):
+
+        for i in range(retry):
+
+            try:
+
+                resp = requests.get(url=url, headers=headers)
+
+                content = resp.text
+
+                md_check = re.findall('summary|lasttradedate', content)
+
+                if content and len(md_check) > 0:
+
+                    return content
+
+                else:
+
+                    time.sleep(60)
+
+                    logger.info('failed to get content, retry: {}'.format(i))
+
+                    continue
+
+            except Exception as e:
+
+                sender_139('获取涨跌停数据出错','{}'.format(e))
+
+                logger.error(e)
+
+                time.sleep(60)
+
+                continue
+
+        return None
+
+
+
+    def convert_json(self, content):
+
+        p = re.compile(r'"Data":(.*)};', re.S)
+
+        if len(content) <= 0:
+
+            logger.info('Content\'s length is 0')
+
+            exit(0)
+
+        result = p.findall(content)
+
+        if result:
+
+            try:
+
+                # print(result)
+
+                t1 = result[0]
+
+                t2=re.sub('[\\r\\n]', '', t1)
+
+                t2=re.sub(',,',',0,0',t2)
+
+                t2 = re.sub('Infinity','-1',t2)
+
+                t2 = re.sub('NaN','-1',t2)
+
+                t2 = list(eval(t2))
+
+                return t2
+
+            except Exception as e:
+
+                sender_139('获取涨跌停数据出错','e{}'.format(e))
+
+                logger.info(e)
+
+                return None
+
+        else:
+
+            return None
+
+
+
+    # 2016-12-27 to do this
+
+    def save_excel(self, date, data):
+
+        # data is list type
+
+        w = xlwt.Workbook(encoding='gbk')
+
+        ws = w.add_sheet(date)
+
+        excel_filename = date + ".xls"
+
+        # sheet=open_workbook(excel_filenme)
+
+        # table=wb.sheets()[0]
+
+        xf = 0
+
+        ctype = 1
+
+        rows = len(data)
+
+        point_x = 1
+
+        point_y = 0
+
+        ws.write(0, 0, '代码')
+
+        ws.write(0, 1, '名称')
+
+        ws.write(0, 2, '最新价格')
+
+        ws.write(0, 3, '涨跌幅')
+
+        ws.write(0, 4, '封成比')
+
+        ws.write(0, 5, '封流比')
+
+        ws.write(0, 6, '封单金额')
+
+        ws.write(0, 7, '第一次涨停时间')
+
+        ws.write(0, 8, '最后一次涨停时间')
+
+        ws.write(0, 9, '打开次数')
+
+        ws.write(0, 10, '振幅')
+
+        ws.write(0, 11, '涨停强度')
+
+        print("Rows:%d" % rows)
+
+        for row in data:
+
+            rows = len(data)
+
+            cols = len(row)
+
+            point_y = 0
+
+            for col in row:
+
+                # print(col)
+
+                # table.put_cell(row,col,)
+
+                # print(col)
+
+                ws.write(point_x, point_y, col)
+
+                # print("[%d,%d]" % (point_x, point_y))
+
+                point_y = point_y + 1
+
+
+
+            point_x = point_x + 1
+
+
+
+        w.save(excel_filename)
+
+
+
+    def save_to_dataframe(self, data, indexx, choice, post_fix):
+
+        engine = get_engine('db_zdt')
+
+        if not data:
+
+            exit()
+
+        data_len = len(data)
+
+        if choice == 1:
+
+            for i in range(data_len):
+
+                data[i][choice] = data[i][choice]
+
+
+
+        df = pd.DataFrame(data, columns=indexx)
+
+
+
+        filename = os.path.join(
+
+            self.path, self.today + "_" + post_fix + ".xls")
+
+
+
+        # 今日涨停
+
+        if choice == 1:
+
+            df['今天的日期'] = self.today
+
+            df.to_excel(filename, encoding='gbk')
+
+            try:
+
+                df.to_sql(self.today + post_fix, engine, if_exists='fail')
+
+            except Exception as e:
+
+                logger.info(e)
+
+        # 昨日涨停
+
+        if choice == 2:
+
+            df = df.set_index('序号')
+
+            df['最大涨幅'] = df['最大涨幅'].map(lambda x: round(x * 100, 3))
+
+            df['最大跌幅'] = df['最大跌幅'].map(lambda x: round(x * 100, 3))
+
+            df['今日开盘涨幅'] = df['今日开盘涨幅'].map(lambda x: round(x * 100, 3))
+
+            df['昨日涨停强度'] = df['昨日涨停强度'].map(lambda x: round(x, 0))
+
+            df['今日涨停强度'] = df['今日涨停强度'].map(lambda x: round(x, 0))
+
+            try:
+
+                df.to_sql(self.today + post_fix, engine, if_exists='fail')
+
+            except Exception as e:
+
+                logger.info(e)
+
+
+
+            avg = round(df['今日涨幅'].mean(), 2)
+
+            median = round(df['今日涨幅'].median(), 2)
+
+            min_v = round(df['今日涨幅'].min(), 2)
+
+
+
+            current = datetime.datetime.now().strftime('%Y-%m-%d')
+
+            title = '昨天涨停个股今天{}\n的平均涨幅{}\n'.format(current, avg)
+
+            content = '昨天涨停个股今天{}\n的平均涨幅{}\n涨幅中位数{}\n涨幅最小{}\n'.format(current,avg,median,min_v)
+
+
+
+            # try:
+
+            #     sender_139(title, content)
+
+            # except Exception as e:
+
+            #     print(e)
+
+
+
+    # 昨日涨停今日的状态，今日涨停
+
+
+
+    def storedata(self):
+
+        zdt_content = self.getdata(self.zdt_url, headers=self.header_zdt)
+
+        # logger.info('zdt Content' + zdt_content)
+
+        zdt_js = self.convert_json(zdt_content)
+
+        self.save_to_dataframe(zdt_js, self.zdt_indexx, 1, 'zdt')
+
+
+
+        # 昨日涨停数据会如果不是当天获取会失效
+
+        time.sleep(0.5)
+
+        zrzt_content = self.getdata(self.zrzt_url, headers=self.header_zrzt)
+
+        logger.info('zrzt Content' + zrzt_content)
+
+
+
+        zrzt_js = self.convert_json(zrzt_content)
+
+        self.save_to_dataframe(zrzt_js, self.zrzt_indexx, 2, 'zrzt')
+
+
+
+
+
+if __name__ == '__main__':
+
+
+
+    # 填补以前的数据
+
+    # date_list = [i for i in list(pd.date_range('20180921','20190101'))]
+
+    # conn = get_mysql_conn('db_zdt','local')
+
+    # cursor = conn.cursor()
+
+    # for d in date_list:
+
+    #     x=datetime.datetime.strftime(d, '%Y%m%d')
+
+    #     y=datetime.datetime.strftime(d, '%Y-%m-%d')
+
+    #     if ts.is_holiday(y):
+
+    #         continue
+
+    #     print(y)
+
+    #     check_cmd ='select 1 from `{}zdt`'.format(x)
+
+    #     try:
+
+    #         cursor.execute(check_cmd)
+
+    #     except Exception as e:
+
+    #         print(e)
+
+    #         obj = GetZDT(x)
+
+    #         obj.storedata()
+
+    #     else:
+
+    #         ret = cursor.fetchone()
+
+    #         print(ret)
+
+    #         continue
+
+
+
+
+
+    # if is_holiday():
+    #
+    #     logger.info('Holiday')
+    #
+    #     exit()
+
+
+
+    logger.info("start")
+
+    obj = GetZDT(None)
+
+    # obj = GetZDT('20200207')
+
+
+
+    obj.storedata()
