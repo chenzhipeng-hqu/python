6ad99982d12806f7eca19dd0bbad1c906e4fd0b2
chenzhipeng3472 <chenzhipeng3472@cvte.com>
2020-06-10 20:08:13 +0800

    [feature][stock][database] add valid_stock function
    [what] 添加 选票函数
    [why]null
    [how]null

--- project/database.py
+++ project/database.py
@@ -27,6 +27,7 @@ logger = log.Log(__name__, log_path=os.getcwd()).getlog()
 class DataBase(object):
     """
     数据库操作
+    https://tushare.pro/document/2
     """
     def __init__(self, start, database):
         logger.debug ('open %s database' % (database))
@@ -42,7 +43,8 @@ class DataBase(object):
             self.__creat_table_allstocks()
             # self.__creat_table_stocks(start='2020-06-03', end='2020-06-05')
             # self.__creat_table_stocks(start='2020-06-01', end='2020-06-04')
-            self.__creat_table_stocks(start=start, end='2020-06-04')
+            # self.__creat_table_stocks(start=start, end='2020-06-04')
+            self.__creat_table_stocks(start=start)
 
         logger.info('max_date: %s' % self.fetch_max_date('000001'))
 
@@ -141,7 +143,8 @@ class DataBase(object):
     def __creat_table_stocks(self, start, end=time.strftime('%Y-%m-%d')):
         # 获取所有有股票
         stock_info = ts.get_stock_basics()
-        codes = sorted(stock_info.index)[:24]
+        # codes = sorted(stock_info.index)[:24]
+        codes = sorted(stock_info.index)
         for code in codes:
             df = ts.get_hist_data(code, start=start, end=end)
 
@@ -195,7 +198,8 @@ class DataBase(object):
         # 获取所有有股票
         stock_info = ts.get_stock_basics()
         # 每次将 000001 放在最后更新, update中判断000001是否最新数据
-        codes = sorted(sorted(stock_info.index)[:24], reverse=True)
+        # codes = sorted(sorted(stock_info.index)[:24], reverse=True)
+        codes = sorted(stock_info.index, reverse=True)
         # codes = sorted(stock_info.index)
         for code in codes:
             cursor = self.conn.execute(
@@ -248,6 +252,8 @@ class DataBase(object):
     def fetch_data(self, code, start, end=None):
         '''
         https://cloud.tencent.com/developer/article/1532914
+        https://www.cnblogs.com/z3286586/p/11845004.html
+        https://www.jb51.net/article/121089.htm
         '''
         # self.cursor.execute('select * from stock_' + code + ' order by date desc limit 1')  # 当天
         self.cursor.execute('select * from stock_' + code + ' where date>=%s and date<=%s' % (start, end))
@@ -331,16 +337,83 @@ class DataBase(object):
         self.update_allstocks()
         self.update_stock_code()
 
+    def valid_stock(self, dates):
+        '''
+        从数据库获取股票数据，统计想要查找日期的满足阳包阴并且当天涨停的股票
+        '''
+        # 先将字符串格式的时间转换为时间格式才能计算昨天的日期
+        now = datetime.date(*map(int,dates.split('-')))
+        oneday = datetime.timedelta(days=1)
+        yestody = str(now - oneday)
+        #将昨天日期转换为规定的字符串格式
+        times = time.strptime(yestody,'%Y-%m-%d')
+        str_yestoday = time.strftime('%Y%m%d',times)
+        logger.info('执行的时间前一天是%s'%str_yestoday)
+        #将想要查找的日期转换为规定的字符串格式
+        str_today = time.strptime(dates,'%Y-%m-%d')
+        today = time.strftime('%Y%m%d',str_today)
+        logger.info('执行的时间是%s'%today)
+        #连接数据库
+        cursor = self.conn.cursor()
+        #查找allstock表获取所有股票代码
+        cursor.execute('select code from allstocks')
+        value_code = cursor.fetchall()
+        a = 0
+        count = []
+        #遍历所有股票
+        for i in range(0,len(value_code)):
+            # if re.match('000',value_code[i][0]) or re.match('002',value_code[i][0]):
+            #查询所有匹配到的股票，将今天与昨天的数据对比
+            try:
+                sql = 'select * from stock_'+ value_code[i][0]+ ' where date=%s or date =%s order by date desc' % (today, str_yestoday)
+                # logger.info(sql)
+                cursor.execute(sql)  #当天
+                #cursor.execute('select * from stock_'+ value_code[i][0]+ ' where date=%s or date =%s'%('20180315','20180314'))
+                value = cursor.fetchall()
+
+                #1是昨天，2是今天
+                #今天的开盘价
+                opens1 = float(value[0][1])
+                #今天的收盘价
+                close1 = float(value[0][2])
+                #今天的涨幅
+                p_change1 = float(value[0][6])
+                #昨天的。。。。。
+                opens2 = float(value[1][1])
+                close2 = float(value[1][2])
+                p_change2 = float(value[1][6])
+
+                #加入这两天的数据满足昨天下跌超过2%，而且今天的开盘价低于昨天的收盘价，且今天的收盘价高于昨天的收盘价，就满足阳包阴的条件
+                if opens2<close1 and close2>opens1 and p_change2<-2 and p_change1>9.8:
+                    # logger.info('%s票%s的开盘价是%s'%(value_code[i][0],today,opens1))
+                    # logger.info('%s票%s的收盘价是%s'%(value_code[i][0],today,close1))
+                    # logger.info('%s票%s的涨幅是%s'%(value_code[i][0],today,p_change1))
+                    # logger.info('%s票%s的开盘价是%s'%(value_code[i][0],str_yestoday,opens2))
+                    # logger.info('%s票%s的收盘价价是%s'%(value_code[i][0],str_yestoday,close2))
+                    # logger.info('%s票%s的涨幅是%s'%(value_code[i][0],str_yestoday,p_change2))
+                    logger.info('%s: %s' % (value_code[i][0], value[0]))
+                    logger.info('%s: %s' % ('      ', value[1]))
+                    # logger.info('%s' % (value[1]))
+                    #将满足条件的股票代码放进列表中，统计当天满足条件的股票
+                    count.append(value_code[i][0])
+                    a += 1
+            except:
+                #之前有次sql语句出错了，order by后面没加date，每次寻找都是0支，找了半个多小时才找出来是sql语句的问题
+                logger.debug('%s停牌无数据,或者请查看sql语句是否正确' % value_code[i][0])#一般不用管，除非执行好多天的数据都为0时那可能输sql语句有问题了
+
+        logger.info('总共找到%d支满足条件的股票:%s' % (a, count))
+        return count, a
 
 if __name__ == '__main__':
     logger.info('\r\n ---------------- welcom to use -----------------')
     db = DataBase(start='2020-06-03', database='./datas/stocks.db')
-    db.fetch_history_data('2020-06-01')
+    # db.fetch_history_data('2019-01-01')
     db.update()
     # value = db.fetch_data('000001', '20200603', '20200605')
 
-    db.export2excel('allstocks')
+    # db.export2excel('allstocks')
     db.export2excel('stock_000001')
 
     # for stock in db.get_all_stocks():
     #     print(stock[0])
+    # db.valid_stock('2020-06-10')
