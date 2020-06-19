7479e3450380f0d18d9636ebd8b08a4b35d7e05d
chenzhipeng3472 <chenzhipeng3472@cvte.com>
2020-06-19 19:21:47 +0800

    [feature][stock][database] add find_gap function
    [what] 添加找缺口方法
    [why]null
    [how]null

--- project/database.py
+++ project/database.py
@@ -394,15 +394,23 @@ class DataBase(object):
                 opens1 = float(value[0][1])
                 #今天的收盘价
                 close1 = float(value[0][2])
+                #今天的成交量
+                volume1 = float(value[0][5])
+                # logger.info(volume1)
                 #今天的涨幅
                 p_change1 = float(value[0][6])
                 #昨天的。。。。。
                 opens2 = float(value[1][1])
                 close2 = float(value[1][2])
+                volume2 = float(value[1][5])
+                # logger.info(volume2)
                 p_change2 = float(value[1][6])
 
                 #加入这两天的数据满足昨天下跌超过2%，而且今天的开盘价低于昨天的收盘价，且今天的收盘价高于昨天的收盘价，就满足阳包阴的条件
-                if opens2 < close1 and close2 > opens1 and p_change2 < -2.5 and p_change1 > 9.8:
+                # if opens2 < close1 and close2 > opens1 and volume1 > (volume2)\
+                if opens2 < close1 and close2 > opens1 \
+                        and p_change2 < -2.5 and p_change1 > 9.8:
+                # if p_change2 < -5.5 and p_change1 > 9.8:
                     # logger.info('%s票%s的开盘价是%s'%(value_code[i][0],today,opens1))
                     # logger.info('%s票%s的收盘价是%s'%(value_code[i][0],today,close1))
                     # logger.info('%s票%s的涨幅是%s'%(value_code[i][0],today,p_change1))
@@ -427,6 +435,95 @@ class DataBase(object):
         logger.info('总共找到%d支满足条件的股票:%s' % (a, count))
         return count, a
 
+    def find_gap(self, dates):
+        '''
+        从数据库获取股票数据，统计想要查找日期的满足阳包阴并且当天涨停的股票
+        '''
+        # 先将字符串格式的时间转换为时间格式才能计算昨天的日期
+        now = datetime.date(*map(int, dates.split('-')))
+        oneday = datetime.timedelta(days=1)
+        yestody = str(now - oneday)
+        #将昨天日期转换为规定的字符串格式
+        times = time.strptime(yestody, '%Y-%m-%d')
+        weekday = time.strftime("%w", times)
+        if weekday is '0':  #如果昨天是周日, 则调整上一个交易日到周五
+            yestody = str(now - oneday - oneday - oneday)
+            #将昨天日期转换为规定的字符串格式
+            times = time.strptime(yestody, '%Y-%m-%d')
+        str_yestoday = time.strftime('%Y%m%d', times)
+        logger.info('执行的时间前一天是%s' % str_yestoday)
+        #将想要查找的日期转换为规定的字符串格式
+        str_today = time.strptime(dates, '%Y-%m-%d')
+        today = time.strftime('%Y%m%d', str_today)
+        logger.info('执行的时间是%s' % today)
+        #连接数据库
+        cursor = self.conn.cursor()
+        #查找allstock表获取所有股票代码
+        cursor.execute('select code,name from allstocks')
+        value_code = cursor.fetchall()
+        # print(value_code)
+        a = 0
+        count = []
+        #遍历所有股票
+        for i in range(0,len(value_code)):
+            # if re.match('000',value_code[i][0]) or re.match('002',value_code[i][0]):
+            #查询所有匹配到的股票，将今天与昨天的数据对比
+            try:
+                sql = 'select * from stock_' + value_code[i][0] \
+                      + ' where date=%s or date=%s order by date desc limit 2' % (today, str_yestoday)
+                # logger.info(sql)
+                cursor.execute(sql)  #当天
+                #cursor.execute('select * from stock_'+ value_code[i][0]+ ' where date=%s or date =%s'%('20180315','20180314'))
+                value = cursor.fetchall()
+
+                #1是昨天，2是今天
+                #今天的开盘价
+                opens1 = float(value[0][1])
+                #今天的收盘价
+                close1 = float(value[0][2])
+                #今天的最低价
+                low1 = float(value[0][2])
+                #今天的成交量
+                volume1 = float(value[0][5])
+                # logger.info(volume1)
+                #今天的涨幅
+                p_change1 = float(value[0][6])
+                #昨天的。。。。。
+                opens2 = float(value[1][1])
+                close2 = float(value[1][2])
+                high2 = float(value[1][3])
+                volume2 = float(value[1][5])
+                # logger.info(volume2)
+                p_change2 = float(value[1][6])
+
+                #加入这两天的数据满足昨天下跌超过2%，而且今天的开盘价低于昨天的收盘价，且今天的收盘价高于昨天的收盘价，就满足阳包阴的条件
+                if low1 > high2 and p_change2 < 0 and p_change1 < 9.8:
+                # if p_change2 < -5.5 and p_change1 > 9.8:
+                    # logger.info('%s票%s的开盘价是%s'%(value_code[i][0],today,opens1))
+                    # logger.info('%s票%s的收盘价是%s'%(value_code[i][0],today,close1))
+                    # logger.info('%s票%s的涨幅是%s'%(value_code[i][0],today,p_change1))
+                    # logger.info('%s票%s的开盘价是%s'%(value_code[i][0],str_yestoday,opens2))
+                    # logger.info('%s票%s的收盘价价是%s'%(value_code[i][0],str_yestoday,close2))
+                    # logger.info('%s票%s的涨幅是%s'%(value_code[i][0],str_yestoday,p_change2))
+                    logger.info('%s: %s' % (value_code[i][0], value[0]))
+                    logger.info('%s: %s' % ('      ', value[1]))
+                    # logger.info('%s' % (value_code[i][1]))
+                    #将满足条件的股票代码放进列表中，统计当天满足条件的股票
+                    # count.append(value_code[i][0])
+                    find = '%s:%s' % (value_code[i][1], value_code[i][0])
+                    count.append(find)
+                    a += 1
+            except:
+                #之前有次sql语句出错了，order by后面没加date，每次寻找都是0支，找了半个多小时才找出来是sql语句的问题
+                logger.debug('%s停牌无数据,或者请查看sql语句是否正确' % value_code[i][0])#一般不用管，除非执行好多天的数据都为0时那可能输sql语句有问题了
+
+        with open('./datas/report_gap/%s.txt' % (today), 'w') as fp:
+            fp.write('总共找到%d支满足条件的股票:%s' % (a, count))
+
+        logger.info('总共找到%d支满足条件的股票:%s' % (a, count))
+        return count, a
+
+
 if __name__ == '__main__':
     logger.info('\r\n ---------------- welcom to use -----------------')
     db = DataBase(start='2020-06-03', database='./datas/stocks.db')
@@ -439,6 +536,10 @@ if __name__ == '__main__':
 
     # for stock in db.get_all_stocks():
     #     print(stock[0])
-    db.valid_stock('2020-06-15')
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    logger.info(today)
+    # db.valid_stock('2020-06-19')
+    db.valid_stock(today)
+    # db.find_gap('2020-06-19')
     # db.get_company('000001')
     # db.get_last_trade_day()
